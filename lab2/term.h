101f7cef593ccad95da16b624e5b28670343e4c0
Direnol <Direnol@yandex.ru>
2017-02-15 23:03:30 +0700

    Init lab2

---
+++ lab2/term.h
@@ -0,0 +1,29 @@
+#ifndef TERM_H
+#define TERM_H
+
+#include <stdint.h>
+#include <stdio.h>
+
+enum ERROR_TERM {
+	ERROR = -1,
+	SUCCES = 0
+}
+
+enum COLORS_TERM {
+	clr_black,
+	clr_red,
+	clr_green,
+	clr_brown,
+	clr_blue,
+	clr_magenta,
+	clr_cyan,
+	clr_light_blue,
+	clr_default
+}
+uint8_t mt_clscr ();
+uint8_t mt_gotoXY(uint16_t, uint16_t);
+uint8_t mt_getscreensize(uint16_t *, uint16_t *);
+uint8_t mt_setfgcolor(enum COLORS_TERM);
+uint8_t	mt_setbgcolor(enum COLORS_TERM);
+
+#endif // TERM_H
